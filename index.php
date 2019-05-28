2d358d6808a952d2285ce8b55adbbe8d58e7192c
muhammad risano akbar <risano@muhammadrisano.sman4padang.sch.id>
2019-05-28 14:19:17 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello Word</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ratione ipsam magnam, tempore tenetur eos sed error consectetur molestias totam optio quis aut alias dicta inventore? Dolor eius sit aut!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
